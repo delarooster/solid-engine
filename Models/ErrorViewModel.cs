505c5049dddf527ddaa9e66ef1cebd719a543319
Austin DeLaRosa <austin.delarosa@gmail.com>
2020-06-08 15:03:03 -0400

    Adding skeleton project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace solid_engine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
